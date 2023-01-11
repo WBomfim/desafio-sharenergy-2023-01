25d0228035cf39b4da3c6acc43f5f5522f5f173d
WBomfim <willianbomfim@hotmail.com>
2023-01-11 16:47:51 -0300

    feat: Realizado o link das rotas para o servidor

--- backend/src/server/app.ts
+++ backend/src/server/app.ts
@@ -1,11 +1,16 @@
 import 'express-async-errors';
 import * as express from 'express';
 import * as cors from 'cors';
+import loginRoutes from './routes/Login';
+import clientRoutes from './routes/Client';
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/login', loginRoutes);
+app.use('/client', clientRoutes);
 
 export default app;
