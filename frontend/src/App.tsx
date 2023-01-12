8e2c80cb877a13106630706076b825b6154ec1a9
WBomfim <willianbomfim@hotmail.com>
2023-01-12 16:32:59 -0300

    feat: Criado as rotas para as páginas da aplicação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,11 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={ <Navigate to="/login" /> } />
-      <Route path="/login" element={  } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/main" element={ <Main /> } />
+      <Route path="/cat-image" element={ <CatImage /> } />
+      <Route path="/dog-image" element={ <DogImage /> } />
+      <Route path="/clients" element={ <Clients /> } />
     </Routes>
   );
 }
