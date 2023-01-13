00b475510d06646f0bdbec5bef9b3256c80d7143
WBomfim <willianbomfim@hotmail.com>
2023-01-13 19:19:33 -0300

    feat: Alterado as funções que lidam com storage para utilizar o localStorage

--- frontend/src/helpers/handleStorage.ts
+++ frontend/src/helpers/handleStorage.ts
@@ -1,72 +1,26 @@
-import minimongo from 'minimongo';
-
-const localDB = new minimongo.LocalStorageDb({namespace: 'Minimongo'}, true);
-
 type User = {
   username: string;
   password: string;
 }
 
-type Token = {
-  token: string;
+export const saveLogin = (user: User): void => {
+  localStorage.setItem('login', JSON.stringify(user));
+};
+
+export const getLogin = (): User => {
+  const user = localStorage.getItem('login');
+  return JSON.parse(user as string);
+};
+
+export const saveToken = (token: string): void => {
+  localStorage.setItem('token', token);
 }
 
-const validateCollection = (user: User): void => {
-  if (!user.password || !user.username) {
-    throw new Error('Invalid user');
-  }
+export const getToken = (): string => {
+  return localStorage.getItem('token') as string
 }
 
-const createUser = (user: User): User => user;
-
-localDB.addCollection('userClient', validateCollection, createUser);
-    
-
-/* type User = {
-  username: string;
-  password: string;
+export const clearStorage = (): void => {
+  localStorage.removeItem('login');
+  localStorage.removeItem('token');
 }
-
-type Token = {
-  token: string;
-}
-
-export const saveUser = async (user: User): Promise<void> => {
-  await localDB.collections["userClient"].remove("login");
-
-  const { username, password } = user;
-  const login = {
-    _id: 'login',
-    username,
-    password,
-  }
-
-  localDB.collections["userClient"].upsert(login);
-}
-
-export const getUser = async (): Promise<User> => {
-  return await localDB.collections["userClient"].findOne({_id: "login"});
-}
-
-export const removeUser = (): void => {
-  localDB.collections["userClient"].remove("login");
-}
-
-export const saveToken = async (token: string): Promise<void> => {
-  await localDB.collections["userClient"].remove("token");
-
-  const tokenLogin = {
-    _id: 'token',
-    token,
-  }
-
-  localDB.collections["userClient"].upsert(tokenLogin);
-}
-
-export const getToken = (): Promise<Token> => {
-  return localDB.collections["userClient"].findOne("token");
-}
-
-export const removeToken = (): void => {
-  localDB.collections["userClient"].remove("token");
-} */
