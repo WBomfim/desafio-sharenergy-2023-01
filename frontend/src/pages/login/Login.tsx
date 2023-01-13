a480ecf85f92389e8e784d9904eaf0708c30750a
WBomfim <willianbomfim@hotmail.com>
2023-01-13 12:09:03 -0300

    feat: Criado a função de login

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,19 +1,29 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { z } from 'zod';
+import { requestRegister } from '../../helpers/handleRequests';
+import { saveToken } from '../../helpers/handleStorage';
 
 const userSchema = z.object({
   username: z.string().min(3),
   password: z.string().min(8)
 });
 
+type token = {
+  token: string;
+};
+
 export default function Login(): JSX.Element {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [disabledButton, setDisabledButton] = useState<boolean>(true);
-  const [failedTryRegister, setFailedTryRegister] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [failedTryLogin, setFailedTryLogin] = useState<boolean>(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const verifyRegisterData = (): void => {
+    const verifyData = (): void => {
       const { success } = userSchema.safeParse({
         username,
         password
@@ -21,8 +31,37 @@ export default function Login(): JSX.Element {
       
       setDisabledButton(!success);
     };
-    verifyRegisterData();
+    verifyData();
   }, [username, password]);
+
+  const login = async () => {
+    setIsLoading(true);
+    setFailedTryLogin(false);
+
+    try {
+      const { token } = await requestRegister<token>("/login", {
+        username,
+        password
+      });
+
+      saveToken(token);
+      navigate('/home');
+
+    } catch (error) {
+      setIsLoading(false);
+      setFailedTryLogin(true);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <main>
+        <section>
+          <h1>Carregando...</h1>
+        </section>
+      </main>
+    );
+  }
 
   return (
     <main>
@@ -52,14 +91,17 @@ export default function Login(): JSX.Element {
             />
           </label>
           <button
-           className='btnlogin'
             type='submit'
             disabled={disabledButton}
+            onClick={(event) => {
+              event.preventDefault();
+              login();
+            }}
           >
             ENTRAR
           </button>
-          <div className='failedLogin'>
-            {failedTryRegister && (
+          <div>
+            {failedTryLogin && (
               <p>Usuário ou senha invalido</p>
             )}
           </div>
