69692a9c4c7fbf199800a139f993cbf4e60b3183
WBomfim <willianbomfim@hotmail.com>
2023-01-13 11:54:38 -0300

    feat: Inserido a função de request com verbo POST

--- frontend/src/helpers/handleRequests.ts
+++ frontend/src/helpers/handleRequests.ts
@@ -4,6 +4,11 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+export const requestRegister = async <T>(rota: string, body: unknown): Promise<T> => {
+  const { data } = await api.post(rota, body);
+  return data;
+};
+
 export const requestData = async <T>(rota: string): Promise<T> => {
   const { data } = await api.get(rota);
   return data;
