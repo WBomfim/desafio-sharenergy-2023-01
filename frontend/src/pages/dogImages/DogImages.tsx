1d6c4ebd56bc82e4070c4fb9598bb409509c65b0
WBomfim <willianbomfim@hotmail.com>
2023-01-14 03:19:19 -0300

    feat: Criado a página DogImage - A página possui um botão de refresh que ao ser acionado atualiza a imagem do cachorro na tela.

--- frontend/src/pages/dogImages/DogImages.tsx
+++ frontend/src/pages/dogImages/DogImages.tsx
@@ -1,7 +1,30 @@
-export default function DogImages() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function DogImages(): JSX.Element {
+  const [url, setUrl] = useState<string>("");
+
+  useEffect(() => {
+    getDogImage();
+  }, []);
+
+  const getDogImage = async () => {
+    const URL = "https://random.dog/woof.json";
+    const { data: { url } } = await axios.get(URL);
+    setUrl(url);
+  };
+
   return (
-    <div>
-      <h1>Dog Images</h1>
-    </div>
+    <main>
+      <div>
+        <button
+          type="button"
+          onClick={() => getDogImage()}
+        >
+          Recarregar
+        </button>
+      </div>
+      <img src={url} alt="dog" />
+    </main>
   );
 }
