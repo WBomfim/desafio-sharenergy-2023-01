7a88aebd3824e2a2e0ce41fedbf9f186029d91c7
WBomfim <willianbomfim@hotmail.com>
2023-01-15 16:09:16 -0300

    style: Estilizado a página de dog-images

--- frontend/src/pages/dogImages/DogImages.tsx
+++ frontend/src/pages/dogImages/DogImages.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import axios from "axios";
+import { Button } from "@mui/material";
 
 export default function DogImages(): JSX.Element {
   const [url, setUrl] = useState<string>("");
@@ -18,16 +19,26 @@ export default function DogImages(): JSX.Element {
   return (
     <>
       <Header />
-      <main>
-        <div>
-          <button
+      <main
+        className="flex flex-col items-center gap-8"
+      >
+        <div
+          className="flex gap-5 mt-8"
+        >
+          <Button
+            className={`bg-teal-600 hover:bg-teal-700 font-bold px-12 py-4
+              shadow-lg text-white`}
             type="button"
             onClick={() => getDogImage()}
           >
             Recarregar
-          </button>
+          </Button>
         </div>
-        <img src={url} alt="dog" />
+        <img
+          src={url}
+          alt="dog"
+          className="w-2/5 max-h-screen  rounded-2xl"
+        />
       </main>
     </>
   );
